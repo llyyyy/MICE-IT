a75ea2d33fd7b90b45de39a83ecda999ea65e349
AppleMint_Git <llyyyy7@gmail.com>
2014-04-22 20:07:03 +0900

    middle-test
    edit

--- middle-test/article.js
+++ middle-test/article.js
@@ -21,7 +21,7 @@ var template  = function(a,b) { // a: template string, b: Object or Array with v
 /**
 	https://github.com/niceaji/javascript-study/blob/gh-pages/doc/ajax.md
 */
-function callAjax(url, callback){
+function callAjax(url, callback) {
 	var xmlhttp;
 	// compatible with IE7+, Firefox, Chrome, Opera, Safari
 	xmlhttp = new XMLHttpRequest();
@@ -34,48 +34,60 @@ function callAjax(url, callback){
 	xmlhttp.send();
 }
 
-function getDom(id){
+function getDom(id) {
 	return document.getElementById(id);
 }
 
-function callbackStudentAjax(responseText){
+function newsTemplateAjax(templateString,articleData,parseString) {
+	// 뉴스 템플
+
+	var news = '';
+	for(var i=0; i<articleData[0].news.length; i++ ){
+		news = articleData[0].news[i];
+		parseString.push( template(templateString, {newsId : articleData[0].news[i].title}  ) );
+	}
+	getDom('newsContents').innerHTML = parseString.join("");
+}
+
+function enterTemplateAjax(templateString,articleData,parseString) {
+	// 연예
+
+	var enter = '';
+	for(var i=0; i<articleData[1].enter.length; i++ ){
+		enter = articleData[1].enter[i];
+		parseString.push( template(templateString, {enterId : articleData[1].enter[i].title}  ) );
+	}
+	getDom('enterContents').innerHTML = parseString.join("");
+}
+
+function sportsTemplateAjax(templateString,articleData,parseString) {
+	// 스포츠
+
+	var sports = '';
+	for(var i=0; i<articleData[2].sports.length; i++ ){
+		sports = articleData[2].sports[i];
+		parseString.push( template(templateString, {sportsId : articleData[2].sports[i].title}  ) );
+	}
+	getDom('sportsContents').innerHTML = parseString.join("");
+}
+
+function callbackArticleAjax(responseText) {
 
 	// string 을 json 으로 변환
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 	var articleData = JSON.parse(responseText);
 	var templateString = getDom('articleTemplate').innerHTML;
-	var parseString;
+	var parseString = [];
 
 	// articleData 분석 및 템플릿에 적용
-	// 뉴스
-	parseString = [];
-	var news = '';
-	for(var i=0; i<articleData[0].news.length; i++ ){
-		news = articleData[0].news[i];
-		parseString.push( template(templateString, {title : articleData[0].news[i].title}  ) );
-	}
-	getDom('newsContents').innerHTML = parseString.join("");
-	// 연예
-	parseString = [];
-	var enter = '';
-	for(var i=0; i<articleData[1].enter.length; i++ ){
-		enter = articleData[1].enter[i];
-		parseString.push( template(templateString, {title : articleData[1].enter[i].title}  ) );
-	}
-	getDom('enterContents').innerHTML = parseString.join("");
-	// 스포츠
-	parseString = [];
-	var sports = '';
-	for(var i=0; i<articleData[2].sports.length; i++ ){
-		sports = articleData[2].sports[i];
-		parseString.push( template(templateString, {title : articleData[2].sports[i].title}  ) );
-	}
-	getDom('sportsContents').innerHTML = parseString.join("");
+	newsTemplateAjax(templateString,articleData,parseString);
+	enterTemplateAjax(templateString,articleData,parseString);
+	sportsTemplateAjax(templateString,articleData,parseString);
 }
 
-function start(){
-	callAjax('articleData.js', callbackStudentAjax);
-	
+function start() {
+	callAjax('articleData.js', callbackArticleAjax);
+
 }
 
 
