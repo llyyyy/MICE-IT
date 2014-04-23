15198e80d7d935eb8fa8283a0b0ab4a555a794b1
AppleMint_Git <llyyyy7@gmail.com>
2014-04-23 21:34:50 +0900

    middle-test
    bootstrap

--- middle-test/article.js
+++ middle-test/article.js
@@ -121,37 +121,40 @@ function callbackArticleAjax(responseText) {
 // 	 	sports[5].remove();
 // }
 
-function showNews(){
+function showNews(e){
 
-	getDom('newsContents').className = "pop";
-	getDom('enterContents').className = "hide";
-	getDom('sportsContents').className = "hide";
-
-	news = getDom('newsContents').children;
+	// getDom('newsContents').className = "pop";
+	// getDom('enterContents').className = "hide";
+	// getDom('sportsContents').className = "hide";
+	e.preventDefault()
+	$([getDom('newsContents')]).tab('show');
+	var news = getDom('newsContents').children;
 	for(var i = 5, length = news.length ; i < length; i++) {
 	 	news[5].remove();
 	}
 }
 
-function showEnter(){
+function showEnter(e){
 
-	getDom('newsContents').className = "hide";
-	getDom('enterContents').className = "pop";
-	getDom('sportsContents').className = "hide";
-
-	enter = getDom('enterContents').children;
+	// getDom('newsContents').className = "hide";
+	// getDom('enterContents').className = "pop";
+	// getDom('sportsContents').className = "hide";
+	e.preventDefault()
+	$([getDom('enterContents')]).tab('show');
+	var enter = getDom('enterContents').children;
 	for(var i = 5, length = enter.length ; i < length; i++) {
 	 	enter[5].remove();
 	}
 }
 
-function showSports(){
+function showSports(e){
 
-	getDom('newsContents').className = "hide";
-	getDom('enterContents').className = "hide";
-	getDom('sportsContents').className = "pop";
-
-	sports = getDom('sportsContents').children;
+	// getDom('newsContents').className = "hide";
+	// getDom('enterContents').className = "hide";
+	// getDom('sportsContents').className = "pop";
+	e.preventDefault()
+	$([getDom('sportsContents')]).tab('show');
+	var sports = getDom('sportsContents').children;
 	for(var i = 5, length = sports.length ; i < length; i++) {
 	 	sports[5].remove();
 	}
