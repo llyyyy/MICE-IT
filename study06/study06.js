100437e77e16cfac92e17225a57ca8aad7b3ed88
AppleMint_Git <llyyyy7@gmail.com>
2014-05-02 16:27:36 +0900

    study06 효과 추

--- study06/study06.js
+++ study06/study06.js
@@ -1,24 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	$('#go').on("click", toggleMove);
-	//$(window).resize(move);
+	
 });
 
-function toggleMove(){
+function toggleMove() {
 	var w_width = $(window).width();
 	var w_height = $(window).height();
 	
 	var $box = $('#box');
+	var $moved = $('.moved');
 	
 	if(!$(box).hasClass("moved")) {
-		$box.animate({left : w_width - $(box).width(),
-			top : w_height - $(box).height() },
- 			1000);
+		$box.animate ({
+			opacity : 0.5,
+			left : w_width - $(box).width(),
+			top : w_height - $(box).height(),
+			fontSize : "30px"
+			}, 2000);
 	}
 	else {
-		$box.animate({left : 10,
-			top : 10 },
- 			1000);
+		$box.animate ({
+			opacity : 1,
+			left : 10,
+			top : 10,
+			fontSize : "15px"
+			}, 2000);
 	}
 	
 	$box.toggleClass("moved");
